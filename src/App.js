17e587737106f760e1a18ef37e34377e923bb6c8
Luciano Ramos Sizilio <01713732432@ifrn.local>
2018-11-07 08:18:23 -0300

    Bug em QBoxButton Resolvido

--- src/App.js
+++ src/App.js
@@ -16,11 +16,16 @@ class App extends Component {
           <HeaderItem button text='Notificações' icon='bell' key='3'/>
           <HeaderItem button text='Turmas' icon='team' key='4'/>
         </Header>
-        <Content>
+        <Content style={{backgroundColor:'#FFFFFF'}}>
           <Carousel>
             <NoticiaCarousel title='Novidades' img={imgteste} subtitle='LLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper, dolor eu finibus dapibus, justo neque blandit dolor, eu lobortis est augue sed orci. Sed tincidunt viverra dolor, sed maximus lacus convallis sed. Quisque vestibulum dictum mauris nec malesuada. Proin luctus id purus a commodo.orem isadlk asldk çlsakd kasdk çsa asjdklasj dsakj dlasjdlka sjdl sjadlksaj ldkjaslkd jsalkd jsalk dj'/>
             <NoticiaCarousel title='Novidades' subtitle='LLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper, dolor eu finibus dapibus, justo neque blandit dolor, eu lobortis est augue sed orci. Sed tincidunt viverra dolor, sed maximus lacus convallis sed. Quisque vestibulum dictum mauris nec malesuada. Proin luctus id purus a commodo.orem isadlk asldk çlsakd kasdk çsa asjdklasj dsakj dlasjdlka sjdl sjadlksaj ldkjaslkd jsalkd jsalk dj'/>
           </Carousel>
+          <br/>
+          <QBoxButton text='teste' color='#14a76c'/>
+          <QBoxButton text='teste' icon="right" color='#14a76c'/>
+          <QBoxButton text='teste' icon="team" color='#14a76c'/>
+          <QBoxButton icon="right" color='#14a76c'/>
           {/* <Noticia title='Novidades' subtitle='LLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper, dolor eu finibus dapibus, justo neque blandit dolor, eu lobortis est augue sed orci. Sed tincidunt viverra dolor, sed maximus lacus convallis sed. Quisque vestibulum dictum mauris nec malesuada. Proin luctus id purus a commodo.orem isadlk asldk çlsakd kasdk çsa asjdklasj dsakj dlasjdlka sjdl sjadlksaj ldkjaslkd jsalkd jsalk dj'/>
            <Noticia img={imgteste} title='Novidades' subtitle='LLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper, dolor eu finibus dapibus, justo neque blandit dolor, eu lobortis est augue sed orci. Sed tincidunt viverra dolor, sed maximus lacus convallis sed. Quisque vestibulum dictum mauris nec malesuada. Proin luctus id purus a commodo.orem isadlk asldk çlsakd kasdk çsa asjdklasj dsakj dlasjdlka sjdl sjadlksaj ldkjaslkd jsalkd jsalk dj'/> 
           <br/>
--- src/components/QBoxButton.js
+++ src/components/QBoxButton.js
@@ -8,7 +8,7 @@ class QBoxButton extends Component {
     renderButton = (text,icon,className) => {
         return (
                 <Button style={this.mainStyle()} className={className}>
-                    {text?<Button style={this.buttonStyle()}>{text}</Button>:null}
+                    {/* {text?<Button style={this.buttonStyle()}>{text}</Button>:null}
                     {icon?
                         <Button style={this.iconStyle()}>
                             <img 
@@ -17,6 +17,16 @@ class QBoxButton extends Component {
                                 alt={icon}
                             />
                         </Button>
+                        :null} */}
+                        {text?<div style={this.buttonStyle()}>{text}</div>:null}
+                    {icon?
+                        <div style={this.iconStyle()}>
+                            <img 
+                                style={{width:'20px',marginLeft:'-9px'}} 
+                                src={img[icon]}
+                                alt={icon}
+                            />
+                        </div>
                         :null}
                 </Button>
         );
@@ -32,26 +42,51 @@ class QBoxButton extends Component {
 
     buttonStyle = () => {
         return    {
-            backgroundColor:this.props.color?this.props.color:'#575757', 
-            borderColor:this.props.color?this.props.color:'#575757',
+            backgroundColor:this.props.color?this.props.color:'#575757',
+            padding:'2px 16px',
             borderRadius:this.props.icon?'5px 0px 0px 5px':'5px 5px 5px 5px', 
+            borderColor:this.props.color?this.props.color:'#575757',
             fontSize:19, 
             color:this.props.textcolor?this.props.textcolor:'#EFEFEF',
-            float:this.props.Noticia?'left':''
+            float:'left'
         };
       }
     iconStyle = () => {
         return {
-            backgroundColor:this.props.color?this.props.color:'#575757', 
-            borderColor:this.props.color?this.props.color:'#575757',
+            backgroundColor:this.props.color?this.props.color:'#575757',
+            padding:this.props.Noticia?'6px 16px':'2px 16px',
             borderRadius:this.props.text?'0px 5px 5px 0px':'5px 5px 5px 5px',
+            borderColor:this.props.color?this.props.color:'#575757',
             opacity:0.8,
             fontSize:19, 
             fontWeight:"bolder",
             width: '20px',
-            float:this.props.Noticia?'right':''
+            float:'right'
         };
     }
+
+    // buttonStyle = () => {
+    //     return    {
+    //         backgroundColor:this.props.color?this.props.color:'#575757', 
+    //         borderColor:this.props.color?this.props.color:'#575757',
+    //         borderRadius:this.props.icon?'5px 0px 0px 5px':'5px 5px 5px 5px', 
+    //         fontSize:19, 
+    //         color:this.props.textcolor?this.props.textcolor:'#EFEFEF',
+    //         float:this.props.Noticia?'left':''
+    //     };
+    //   }
+    // iconStyle = () => {
+    //     return {
+    //         backgroundColor:this.props.color?this.props.color:'#575757', 
+    //         borderColor:this.props.color?this.props.color:'#575757',
+    //         borderRadius:this.props.text?'0px 5px 5px 0px':'5px 5px 5px 5px',
+    //         opacity:0.8,
+    //         fontSize:19, 
+    //         fontWeight:"bolder",
+    //         width: '20px',
+    //         float:this.props.Noticia?'right':''
+    //     };
+    // }
 
     render()  {
         return (
