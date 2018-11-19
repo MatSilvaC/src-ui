fe7acdda0eab402a6604aa4a7e623abf2cba79c4
Leonardo MINORA <leonardo.minora@gmail.com>
2018-11-19 18:11:38 -0300

    adicionado script para setar usuário do git

---
+++ config/setgit-luciano.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git config --global --unset user.name
+git config --global --unset user.email
+git config user.name "Luciano Sizilio"
+git config user.email "lucianorsizilio@gmail.com"
---
+++ config/setgit-matheus.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git config --global --unset user.name
+git config --global --unset user.email
+git config user.name "Matheus Silva"
+git config user.email "matheusgmer@hotmail.com"
