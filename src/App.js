f0c3affb56fad66c4e5457e8496a4a95334a6043
Matheus Silva <matheusgmer@hotmail.com>
2018-11-19 18:46:41 -0300

    Adicionar o sider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Carousel, Noticia, NoticiaCarousel, QBoxButton, Header, HeaderItem, Cartao} from "./components";
+import {Carousel, Noticia, QBoxSider, NoticiaCarousel, QBoxButton, Header, HeaderItem, Cartao} from "./components";
 import { Layout } from 'antd';
+
 
 const {Content} = Layout;
 const imgteste ='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png';
 class App extends Component {
   render() {
     return (
+      <Layout>
+      <QBoxSider />
       <Layout>
         <Header color='#653998'>
           <HeaderItem image icon='menu' text='menu' left />
@@ -23,6 +26,7 @@ class App extends Component {
             <NoticiaCarousel title='Novidades' subtitle='LLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper, dolor eu finibus dapibus, justo neque blandit dolor, eu lobortis est augue sed orci. Sed tincidunt viverra dolor, sed maximus lacus convallis sed. Quisque vestibulum dictum mauris nec malesuada. Proin luctus id purus a commodo.orem isadlk asldk çlsakd kasdk çsa asjdklasj dsakj dlasjdlka sjdl sjadlksaj ldkjaslkd jsalkd jsalk dj'/>
           </Carousel>
         </Content>
+        </Layout>
       </Layout>
     );
   }
--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import NoticiaCarousel from "./NoticiaCarousel";
 import QBoxButton from "./QBoxButton";
 import Header from "./Header";
 import HeaderItem from "./HeaderItem";
+import QBoxSider from "./QBoxSider";
 
 
-export { Cartao, Carousel, Noticia, NoticiaCarousel, QBoxButton, Header, HeaderItem};
+export { Cartao,QBoxSider, Carousel, Noticia, NoticiaCarousel, QBoxButton, Header, HeaderItem};
