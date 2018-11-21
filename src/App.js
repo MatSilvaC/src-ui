cba1e9b0fd5665c5c59724d4741489b0cba5b8d0
LuSizilio <lucianorsizilio@gmail.com>
2018-11-21 13:22:44 -0300

    Fazendo cartao de questao

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import './App.css';
 import {Carousel, NoticiaCarousel, Header, HeaderItem, QBoxDrawer, Cartao} from "./components";
-=======
-import {Carousel, NoticiaCarousel, Header, HeaderItem, QBoxDrawer} from "./components";
->>>>>>> dbe05fe1e9af3a6980ae9d9afb98a2e4e71e05e7
 import { Layout } from 'antd';
 import './App.css';
 
@@ -34,7 +30,8 @@ class App extends Component {
               <NoticiaCarousel title='Novidades' subtitle='LLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper, dolor eu finibus dapibus, justo neque blandit dolor, eu lobortis est augue sed orci. Sed tincidunt viverra dolor, sed maximus lacus convallis sed. Quisque vestibulum dictum mauris nec malesuada. Proin luctus id purus a commodo.orem isadlk asldk çlsakd kasdk çsa asjdklasj dsakj dlasjdlka sjdl sjadlksaj ldkjaslkd jsalkd jsalk dj'/>
               <NoticiaCarousel class='tes1' title='Novidades' img={imgteste} subtitle='LLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper, dolor eu finibus dapibus, justo neque blandit dolor, eu lobortis est augue sed orci. Sed tincidunt viverra dolor, sed maximus lacus convallis sed. Quisque vestibulum dictum mauris nec malesuada. Proin luctus id purus a commodo.orem isadlk asldk çlsakd kasdk çsa asjdklasj dsakj dlasjdlka sjdl sjadlksaj ldkjaslkd jsalkd jsalk dj'/>
             </Carousel>
-            <Cartao text="asdasdasdasndsa,das"/>
+            <Cartao text="LLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper, dolor eu finibus dapibus, justo neque blandit dolor, eu lobortis est augue sed orci. Sed tincidunt viverra dolor, sed maximus lacus convallis sed. Quisque vestibulum dictum mauris nec malesuada. Proin luctus id purus a commodo.orem isadlk asldk çlsakd kasdk çsa asjdklasj dsakj dlasjdlka sjdl sjadlksaj ldkjaslkd jsalkd jsalk dj"/>
+            <br/>
           </Content>
         </Layout>
     );
--- src/components/Cartao.js
+++ src/components/Cartao.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import '../App.css';
 
 import 'antd/dist/antd.css';
-import { Card, Icon, Divider } from 'antd';
+import { Card, Divider, Rate } from 'antd';
 const { Meta } = Card;
 
 class Cartao extends Component {
   renderCard = (text) => {
     return (
       <Card
-        style={{width:400,borderRadius:'10px 0px 0px 0px',backgroundColor:'#f79810'}}
-        
-        cover={<div><div style={{height:20,borderRadius:'10px 0px 0px 0px'}}></div><img alt="imagem" style={{width:'100%'}} src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" /></div>}
-        actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
+        style={{width:300,borderRadius:'10px 0px 10px 0px',borderWidth:0,backgroundColor:'#f79810',margin:5}}
+        cover={<div><div style={{height:20}}></div><img alt="imagem" style={{width:'100%'}} src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" /></div>}
+        actions={[<Rate/>]}
       >
         <Meta
           // avatar={
@@ -20,9 +19,9 @@ class Cartao extends Component {
           //       <Icon type={!done?"close":"check" } style={!done?{color:"#d9534f"}:{color:"#5cb85c"}} theme="outlined" />
           //   </Avatar>
           // }
-          description={text}
+          description={[<div style={{color:'#FFFFFF',fontSize:15,lineHeight:'15px'}}>{text}</div>]}
         />
-        <Divider/>
+        <Divider style={{marginBottom:-26,backgroundColor:'#494949'}}/>
       </Card>
     );
   }
